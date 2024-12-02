5344541a8370c27c4d6d6c451baa28ab17d81b6b
RuzitoTV <150984937+RuzitoTV@users.noreply.github.com>
2024-12-01 22:03:40 -0300

    Update Servomotor.ino

--- Servomotor.ino/Servomotor.ino
+++ Servomotor.ino/Servomotor.ino
@@ -3,35 +3,35 @@
 Servo myServo;
 
 void setup() {
-   myServo.attach(2); // Pin donde está conectado el servomotor
+   myServo.attach(2); 
    Serial.begin(9600);
 }
 
 void loop() {
    abrirPuerta();
-   delay(5000); // Espera 5 segundos antes de cerrar la puerta
+   delay(5000); 
    cerrarPuerta();
-   delay(5000); // Espera 5 segundos antes de volver a iniciar el ciclo
+   delay(5000); 
 }
 
 void abrirPuerta() {
    Serial.println("Abriendo puerta...");
-   int angulo = 90; // Comienza en la posición cerrada (90 grados)
+   int angulo = 90; 
    do {
-      angulo = angulo - 1; // Disminuye el ángulo para abrir
+      angulo = angulo - 1; 
       myServo.write(angulo);
-      delay(50); // Control de velocidad
+      delay(50); 
    } while (angulo > 0);
    Serial.println("Puerta abierta.");
 }
 
 void cerrarPuerta() {
    Serial.println("Cerrando puerta...");
-   int angulo = 0; // Comienza en la posición abierta (0 grados)
+   int angulo = 0; 
    do {
-      angulo = angulo + 1; // Incrementa el ángulo para cerrar
+      angulo = angulo + 1; 
       myServo.write(angulo);
-      delay(50); // Control de velocidad
+      delay(50); 
    } while (angulo < 90);
    Serial.println("Puerta cerrada.");
 }
